31bc7b12d519b29e658f63ef7c14a45b658c6c99
Sebastian <wahani@gmail.com>
2019-02-28 14:55:55 +0100

    fix in removeAllfiles

--- R/removeAllFiles.R
+++ R/removeAllFiles.R
@@ -11,13 +11,8 @@ removeAllFiles <- function(db, ...) {
 #' @rdname rsync
 #' @export
 removeAllFiles.default <- function(db, verbose = FALSE, ...) {
-
-  if(verbose == TRUE) args <-  "-rvv --delete"
-  else args <- "-r --delete"
-
   dat <- listFiles(db)
   entries <- dat$name
-
   lapply(entries, removeFile, db = db, verbose = verbose)
   db
 
