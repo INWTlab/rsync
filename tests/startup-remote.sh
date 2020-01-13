51bb02f99624e776782581c837f0b29c1741c653
Sebastian Warnholz <wahani@gmail.com>
2020-01-13 13:30:01 +0100

    RSYNC_CONNECT_PROG as sshProg in rsync

--- tests/startup-remote.sh
+++ tests/startup-remote.sh
@@ -18,3 +18,17 @@ docker cp ./docker-root.pub test-sshd:/root/.ssh/authorized_keys
 docker exec test-sshd chown root:root /root/.ssh/authorized_keys
 docker exec test-sshd apt-get update
 docker exec test-sshd apt-get install -y rsync
+
+echo "starting sshd and rsyncd in container"
+docker run \
+       -d \
+       -p 20012:22 \
+       --name test-sshd-rsyncd \
+       rastasheep/ubuntu-sshd:18.04
+docker cp ./docker-root.pub test-sshd-rsyncd:/root/.ssh/authorized_keys
+docker exec test-sshd-rsyncd chown root:root /root/.ssh/authorized_keys
+docker exec test-sshd-rsyncd apt-get update
+docker exec test-sshd-rsyncd apt-get install -y rsync netcat
+docker cp ./daemon-startup.sh test-sshd-rsyncd:/daemon-startup.sh
+docker exec test-sshd-rsyncd bash /daemon-startup.sh rsync_server
+
