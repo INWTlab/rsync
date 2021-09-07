837fd92eee9f09ef4bfcfc205c65445574a1793c
Sebastian Warnholz <wahani@gmail.com>
2021-09-07 09:16:52 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,13 @@ set -o errexit -o nounset
 addToDrat(){
   PKG_REPO=$PWD
 
-  cd ..; mkdir drat; cd drat
-
-  ## Set up Repo parameters
-  git init
+  cd ..
+  git clone https://$GH_TOKEN@github.com/inwtlab/drat.git
   git config user.name "Travis CI"
   git config user.email "sebastian.warnholz@inwt-statistics.de"
   git config --global push.default simple
 
-  ## Get drat repo
-  git remote add upstream "https://$GH_TOKEN@github.com/inwtlab/drat.git"
-  git fetch upstream 2>err.txt
-  cd docs
-
+  cd drat/docs
   Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
     repodir = '.', \
     commit='Travis update: build $TRAVIS_BUILD_NUMBER')"
