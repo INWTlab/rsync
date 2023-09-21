c73494d3d68facf03b1a8ae8e920540d28d2aa41
Jonathan Bob <jonathan.bob@inwt-statistics.de>
2023-09-21 20:34:25 +0200

    add test case

--- tests/testthat/test-validateFile.R
+++ tests/testthat/test-validateFile.R
@@ -3,6 +3,7 @@ context("validateFile")
 testthat::test_that("validate identical entries", {
 
   con <- setupTestEnvironment()
+  existingTempDirectories <- list.files(tempdir(), pattern = "[[:alpha:]]{8}")
 
   removeAllFiles(con)
   sendFile(con, fileName = 'x.Rdata')
@@ -19,4 +20,16 @@ testthat::test_that("validate identical entries", {
     "Src and dest file are not identical!"
   )
 
+  # This test needs at least some documentation. We encountered an issue with already existing
+  # directoris when running dir.create(db1$src) in validateFile. So, we are calling unlink in the
+  # exit code of validateFile. To ensure that the directory has been removed, we would usually call
+  # dir.exists but we don't know the path name as the directory was created inside of validateFile.
+  # So, we store all the directories inside of tempdir before we call validateFile and do the same
+  # at the end of the test. If the directory was actually unlinked, we expect the set of
+  # directories inside tempdir to remain unchanged.
+  testthat::expect_identical(
+    existingTempDirectories,
+    list.files(tempdir(), pattern = "[[:alpha:]]{8}")
+  )
 })
+
